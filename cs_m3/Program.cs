e66506cc3f24ffc264e3227a08ad2ec5739d01ec
Ivan Grechkin <cobreg666@gmail.com>
2023-07-13 15:50:39 +0300

    First task completed

--- cs_m3/Program.cs
+++ cs_m3/Program.cs
@@ -7,6 +7,22 @@ using System.Threading.Tasks;
 namespace cs_m3 {
     internal class Program {
         static void Main(string[] args) {
+
+            #region Task 1
+
+            Console.Write("Please enter a number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if(a % 2 == 0) {
+                Console.WriteLine($"Число {a} четное");
+            } else {
+                Console.WriteLine($"Число {a} не чётное");
+            }
+
+            Console.ReadKey();
+
+            #endregion
+
         }
     }
 }
