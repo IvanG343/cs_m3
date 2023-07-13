7e6598392c90797a156545fa40f1f48d295276c0
Ivan Grechkin <cobreg666@gmail.com>
2023-07-13 17:54:14 +0300

    Task 3 completed

--- cs_m3/Program.cs
+++ cs_m3/Program.cs
@@ -13,7 +13,7 @@ namespace cs_m3 {
             //Console.Write("Пожалуйста введите число: ");
             //int a = int.Parse(Console.ReadLine());
 
-            //if (a % 2 == 0) {
+            //if(a % 2 == 0) {
             //    Console.WriteLine($"Число {a} четное");
             //} else {
             //    Console.WriteLine($"Число {a} не чётное");
@@ -26,43 +26,76 @@ namespace cs_m3 {
 
             #region Task 2
 
-            int cardSum = 0;
+            //int cardSum = 0;
 
-            Console.WriteLine("\nПриветсвтвуем в нашем казино!");
-            Console.Write($"Укажите количество карт у вас на руках: ");
-            int cardCount = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Приветсвтвуем в нашем казино!");
+            //Console.Write("Укажите количество карт у вас на руках: ");
+            //int cardCount = int.Parse(Console.ReadLine());
 
-            for (int i = 1; i <= cardCount;) {
-                Console.Write($"Введите номинал {i} карты (используйте значение J, Q, K и T для картинок): ");
-                string value = Console.ReadLine();
-                bool isInteger = int.TryParse(value, out int num);
+            //for (int i = 1; i <= cardCount;) {
+            //    Console.Write($"Введите номинал {i} карты (используйте значение J, Q, K и T для картинок): ");
+            //    string value = Console.ReadLine();
+            //    bool isInteger = int.TryParse(value, out int num);
 
-                if (isInteger && num >= 2 && num <= 10) {
-                    cardSum += num;
-                    i++;
-                } else {
-                    switch(value) {
-                        case "J":
-                        case "j":
-                        case "Q":
-                        case "q":
-                        case "K":
-                        case "k":
-                        case "T":
-                        case "t":
-                            cardSum += 10;
-                            i++;
-                            break;
-                        default:
-                            Console.WriteLine("Указан неверный номинал карты");
-                            break;
-                    }
-                }
-            }
+            //    if (isInteger && num >= 2 && num <= 10) {
+            //        cardSum += num;
+            //        i++;
+            //    } else {
+            //        switch (value) {
+            //            case "J":
+            //            case "j":
+            //            case "Q":
+            //            case "q":
+            //            case "K":
+            //            case "k":
+            //            case "T":
+            //            case "t":
+            //                cardSum += 10;
+            //                i++;
+            //                break;
+            //            default:
+            //                Console.WriteLine("Указан неверный номинал карты");
+            //                break;
+            //        }
+            //    }
+            //}
 
-            Console.WriteLine($"Вы набрали {cardSum} очков!");
+            //Console.WriteLine($"Вы набрали {cardSum} очков!");
 
-            Console.ReadKey();
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Task 3
+
+            //bool isComposite = false;
+            //bool isNum;
+            //int counter = 2;
+            //int number;
+
+            //do {
+            //    Console.Write("Введите число больше единцы: ");
+            //    isNum = int.TryParse(Console.ReadLine(), out number);
+            //    if (!isNum || number <= 1) {
+            //        Console.WriteLine("Вы ввели неверное число!\n");
+            //    }
+            //} while (!isNum || number <= 1);
+
+            //while (counter < number) {
+            //    if (number % counter == 0) {
+            //        isComposite = true;
+            //        break;
+            //    }
+            //    counter++;
+            //}
+
+            //Console.WriteLine(isComposite ? $"Число {number} не является простым числом" : $"{number} простое число");
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Task 4
 
             #endregion
 
